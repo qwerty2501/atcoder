01db76c858d9dafff2580263193d3997679377ee
qwerty2501 <qwerty2501@live.jp>
2020-08-04 01:41:11 +0900

    solved abc167 d.

--- abc167/src/bin/d.rs
+++ abc167/src/bin/d.rs
@@ -1,3 +1,147 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::*;
+
+macro_rules! test {
+    ($($input:expr => $output:expr),* $(,)*) => {
+#[test]
+        fn solve_test() {
+            $(
+                {
+                    let mut out = Vec::<u8>::new();
+                    solve($input,&mut out);
+                    let out_str =str::from_utf8(&out).unwrap();
+                    assert_eq!(&out_str, &$output);
+                }
+            )*
+        }
+    };
+}
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+#[allow(unused_mut)]
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr,str) => {
+        $iter.next().unwrap()
+    };
+
+    ($iter:expr, $t:tt) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+macro_rules! stdin {
+    () => {{
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    }};
+}
+
 fn main() {
-    unimplemented!();
+    let mut out = std::io::stdout();
+
+    solve(&stdin!(), &mut out);
+}
+
+#[allow(unused_mut)]
+fn solve<W: std::io::Write>(src: &str, out: &mut W) {
+    #[allow(unused_macros)]
+    macro_rules! out {
+        ($fmt:expr) => {{
+            let _ = out.write_fmt(format_args!($fmt));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            let _ = out.write_fmt(format_args!($fmt, $($args)*));
+        }};
+    }
+    #[allow(unused_macros)]
+    macro_rules! outln {
+        ($fmt:expr) => {{
+            out!(concat!($fmt,"\n"));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            out!(concat!($fmt, "\n"), $($args)*);
+        }};
+    }
+
+    input! {
+        source = src,
+        n:usize,
+        k:usize,
+        a:[usize1;n],
+    }
+    let mut m = HashMap::new();
+    m.insert(0, 0);
+    let mut c = 0;
+    let mut i = 0;
+    let mut looped = false;
+    while c < k {
+        i = a[i];
+        c += 1;
+        if !looped {
+            if m.contains_key(&i) {
+                let mi = m[&i];
+                let s = c - mi;
+                k = (k - mi) % s;
+                c = 0;
+                looped = true;
+            } else {
+                m.insert(i, c);
+            }
+        }
+    }
+    out!("{}", i + 1);
+}
+test! {
+    include_str!("d1_in.txt") => "3",
+    include_str!("d2_in.txt") => "2",
+
 }
