beeed9de671fc700cbecd4280341d08a00c21093
qwerty2501 <qwerty2501@live.jp>
2020-08-16 01:20:18 +0900

    solved abc175

---
+++ abc175/src/bin/a.rs
@@ -0,0 +1,135 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::*;
+
+macro_rules! test {
+    ($($input:expr => $output:expr),* $(,)*) => {
+#[test]
+        fn solve_test() {
+            $(
+                {
+                    let mut out = Vec::<u8>::new();
+                    solve($input,&mut out);
+                    let out_str =str::from_utf8(&out).unwrap();
+                    assert_eq!(&out_str, &$output);
+                }
+            )*
+        }
+    };
+}
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+#[allow(unused_mut)]
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr,str) => {
+        $iter.next().unwrap()
+    };
+
+    ($iter:expr, $t:tt) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+macro_rules! stdin {
+    () => {{
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    }};
+}
+
+fn main() {
+    let mut out = std::io::stdout();
+
+    solve(&stdin!(), &mut out);
+}
+
+#[allow(unused_mut)]
+fn solve<W: std::io::Write>(src: &str, out: &mut W) {
+    #[allow(unused_macros)]
+    macro_rules! out {
+        ($fmt:expr) => {{
+            let _ = out.write_fmt(format_args!($fmt));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            let _ = out.write_fmt(format_args!($fmt, $($args)*));
+        }};
+    }
+    #[allow(unused_macros)]
+    macro_rules! outln {
+        ($fmt:expr) => {{
+            out!(concat!($fmt,"
+"));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            out!(concat!($fmt, "
+"), $($args)*);
+        }};
+    }
+
+    input! {
+        source = src,
+        s:chars,
+    }
+
+    let mut c = 0;
+    let mut m = 0;
+
+    for &si in s.iter() {
+        if si == 'S' {
+            c = 0;
+        } else {
+            c += 1;
+            m = max(m, c);
+        }
+    }
+    out!("{}", m);
+}
+test! {}
---
+++ abc175/src/bin/b.rs
@@ -0,0 +1,146 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::*;
+
+macro_rules! test {
+    ($($input:expr => $output:expr),* $(,)*) => {
+#[test]
+        fn solve_test() {
+            $(
+                {
+                    let mut out = Vec::<u8>::new();
+                    solve($input,&mut out);
+                    let out_str =str::from_utf8(&out).unwrap();
+                    assert_eq!(&out_str, &$output);
+                }
+            )*
+        }
+    };
+}
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+#[allow(unused_mut)]
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr,str) => {
+        $iter.next().unwrap()
+    };
+
+    ($iter:expr, $t:tt) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+macro_rules! stdin {
+    () => {{
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    }};
+}
+
+fn main() {
+    let mut out = std::io::stdout();
+
+    solve(&stdin!(), &mut out);
+}
+
+#[allow(unused_mut)]
+fn solve<W: std::io::Write>(src: &str, out: &mut W) {
+    #[allow(unused_macros)]
+    macro_rules! out {
+        ($fmt:expr) => {{
+            let _ = out.write_fmt(format_args!($fmt));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            let _ = out.write_fmt(format_args!($fmt, $($args)*));
+        }};
+    }
+    #[allow(unused_macros)]
+    macro_rules! outln {
+        ($fmt:expr) => {{
+            out!(concat!($fmt,"
+"));
+        }};
+        ($fmt:expr, $($args:tt)*) => {{
+            out!(concat!($fmt, "
+"), $($args)*);
+        }};
+    }
+
+    input! {
+        source = src,
+        n:usize,
+        l:[usize;n],
+    }
+
+    let mut count = 0;
+    for (i, li) in l.iter().enumerate() {
+        let l = &l[i + 1..];
+        for (j, lj) in l.iter().enumerate() {
+            let l = &l[j + 1..];
+            for (k, lk) in l.iter().enumerate() {
+                if li != lj && li != lk && lj != lk {
+                    let (a, b, &c) = if li > lj && li > lk {
+                        (max(lj, lk), min(lj, lk), li)
+                    } else if lj > lk {
+                        (max(li, lk), min(li, lk), lj)
+                    } else {
+                        (max(li, lj), min(li, lj), lk)
+                    };
+                    if a - b < c && c < a + b {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    out!("{}", count);
+}
+test! {}
